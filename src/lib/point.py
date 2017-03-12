ad6f4b70585c5313c03a3c84d4e01c5b134f0785
Zénon Roy <king-zed@hotmail.com>
2017-03-12 17:29:23 -0400

    Update point.py

--- src/lib/point.py
+++ src/lib/point.py
@@ -2,3 +2,9 @@ class Point():
 	def __init__(self, x = 0, y = 0):
 		self.x = x
 		self.y = y
+		
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
