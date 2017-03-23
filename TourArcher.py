b5f3889c55f13c5c744ba767595326db493269f2
Lee-Stenio nazer <ls_nazer@hotmail.com>
2017-03-23 01:35:39 -0400

    Add files via upload

---
+++ TourArcher.py
@@ -0,0 +1,9 @@
+
+
+class TourArcher():
+    def __init__(self, x, y, speed = 50, attaque = 1):
+        #self.imgTourArcher = tPhotoImage(file = "icones/tour_archer.png")
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.attaque = attaque
