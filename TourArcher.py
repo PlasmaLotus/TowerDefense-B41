6da5905e3e901705143ef467fbb7c84adb48f259
Lee-Stenio nazer <ls_nazer@hotmail.com>
2017-03-23 10:24:17 -0400

    Delete TourArcher.py

--- TourArcher.py
+++
@@ -1,9 +0,0 @@
-
-
-class TourArcher():
-    def __init__(self, x, y, speed = 50, attaque = 1):
-        #self.imgTourArcher = tPhotoImage(file = "icones/tour_archer.png")
-        self.x = x
-        self.y = y
-        self.speed = speed
-        self.attaque = attaque
